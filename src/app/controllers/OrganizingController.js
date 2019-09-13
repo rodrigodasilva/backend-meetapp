5a4438747711d8e4e3b3dfadd3d9d9fca942d56e
rodrigodasilva <ribeiro.rodrigodasilva>
2019-09-13 17:19:58 -0300

    paginacao no index do organizing

--- src/app/controllers/OrganizingController.js
+++ src/app/controllers/OrganizingController.js
@@ -3,7 +3,9 @@ import File from '../models/File';
 
 class OrganizingCrontroller {
   async index(req, res) {
-    const meetups = await Meetup.findAll({
+    const { page = 1 } = req.query;
+
+    const meetups = await Meetup.findAndCountAll({
       where: { user_id: req.userId },
       include: [
         {
@@ -12,6 +14,10 @@ class OrganizingCrontroller {
           attributes: ['id', 'path', 'url'],
         },
       ],
+      order: [['date', 'DESC']],
+
+      limit: 5,
+      offset: (page - 1) * 5,
     });
 
     return res.json(meetups);
